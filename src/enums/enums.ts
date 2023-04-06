94c7181412f5a571261c2bbc5e25b923e4447c78
apella1 <johncollons5@gmail.com>
2023-04-06 23:57:03 +0300

    Add enums type assertions

--- src/enums/enums.ts
+++ src/enums/enums.ts
@@ -1,11 +1,28 @@
-enum LogLevel {
-  DEBUG,
-  INFO,
-  ERROR,
+export enum LogLevel {
+  DEBUG = "DEBUG",
+  INFO = "INFO",
+  ERROR = "ERROR",
 }
 
-const func = (logLevel: LogLevel.ERROR) => {};
-const func2 = (logLevel: LogLevel.ERROR) => {};
+console.log(LogLevel.INFO);
 
-// enums offering a single source of truth as the valuues
+const func = (logLevel: LogLevel.ERROR) => {};
+export const func2 = (logLevel: LogLevel.INFO) => {};
+
+// enums offering a single source of truth as the values
 // can be edited right from the enum declaration
+
+export const shapes = {
+  circle: "circle",
+  square: "square",
+  rectangle: "rectangle",
+  hexagon: "hexagon",
+} as const;
+
+export type Shape = typeof shapes[keyof typeof shapes];
+
+const resolveShape = (shape: Shape) => {};
+
+resolveShape("rectangle")
+
+// ! Drawbacks of enums
