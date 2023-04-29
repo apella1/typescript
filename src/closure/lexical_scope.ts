83fc779dcf6899f3a6ad3ec25d73e7469e9cf1c4
apella1 <johncollons5@gmail.com>
2023-04-29 17:55:17 +0300

    Add persistent lexical scope referenced data

---
+++ src/closure/lexical_scope.ts
@@ -0,0 +1,11 @@
+/**
+ * * Main Concepts
+ * Scope
+ * Lexical scoping/Static scoping
+ * Closure
+ * Persistent Lexical Scope Referenced Data
+ * Closed Over 'Variable Environment'
+ * Dynamic Scoping
+ * */
+
+// ! "If Hemmingway Wrote JavaScript"
