ee1788a73897c10914ef979d764cda43f014816f
apella1 <johncollons5@gmail.com>
2023-04-30 05:44:23 +0300

    Add multiple instances of closure

--- src/closure/lexical_scope.ts
+++ src/closure/lexical_scope.ts
@@ -9,3 +9,21 @@
  * */
 
 // ! "If Hemmingway Wrote JavaScript"
+
+// Multiple Instances Of Closure
+function makeIncrementFrom88() {
+  let num = 8;
+
+  function incrementFrom8() {
+    num++;
+  }
+
+  return incrementFrom8;
+}
+
+const firstIncrementFrom8 = makeIncrementFrom88();
+firstIncrementFrom8();
+firstIncrementFrom8();
+const secondIncrementFrom8 = makeIncrementFrom88();
+secondIncrementFrom8();
+secondIncrementFrom8();
