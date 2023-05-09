819fc4e241a69fd50e6e0869cd53bcba988dd801
apella1 <johncollons5@gmail.com>
2023-05-09 21:43:35 +0300

    Add scope and compiler theory

---
+++ src/closure/scope.ts
@@ -0,0 +1,18 @@
+/**
+ * * Scope
+ * Compiler theory
+ * Engine
+ * RHS 
+ * LHS
+ */
+    
+function log(a: string | number) {
+    console.log(a)
+}
+
+function add (a: number) {
+    let b = 7
+    console.log(a + b)
+}
+
+add(3)
